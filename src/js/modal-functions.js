0a865e8bf723d3250ae8d693e9621cc7ccb336b7
6stix <34929263+nav6maini@users.noreply.github.com>
2020-04-03 22:53:25 -0400

    Modal

--- src/js/modal-functions.js
+++ src/js/modal-functions.js
@@ -1,23 +1,23 @@
 const modalHTML = `
-<div class="modal fade" id="navigation-modal" tabindex="-1" role="dialog"
-  aria-labelledby="navigation-modalTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-          <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLongTitle">Welcome to Cyber Campus!</h5>
-          </div>
-          <div class="modal-body">
-              <p>What's your name?</p>
-              <input type="text" onchange="changeName(this.value)">
-          </div>
-          <div class="modal-footer">
-              <button type="button" class="btn btn-primary" onclick="hideModal()">Save username</button>
-              <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
-              <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
-          </div>
-      </div>
-  </div>
-</div>`;
+  <div class="modal fade" id="navigation-modal" tabindex="-1" role="dialog"
+    aria-labelledby="navigation-modalTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLongTitle">Welcome to Cyber Campus!</h5>
+            </div>
+            <div class="modal-body">
+                <p>What's your name?</p>
+                <input type="text" onchange="changeName(this.value)">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" onclick="hideModal()">Save username</button>
+                <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
+                <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+            </div>
+        </div>
+    </div>
+  </div>`;
 
 function showModal() {
   // maybe check if user is already logged in first...
