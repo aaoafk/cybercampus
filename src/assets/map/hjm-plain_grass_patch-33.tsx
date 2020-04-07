2246c81b22349182a4b4892da5b17bcbf2bcd3a2
rmuniu1 <rmuniu1@swarthmore.edu>
2020-04-06 21:58:10 -0400

    Pushing new maps

--- src/assets/map/hjm-plain_grass_patch-33.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="hjm-plain_grass_patch-33" tilewidth="32" tileheight="32" tilecount="98" columns="14">
- <image source="hjm-plain_grass_patch-33.png" width="458" height="229"/>
-</tileset>
