786ceb6d59d75045de2d199f9f103ed92ebaa003
6stix <34929263+nav6maini@users.noreply.github.com>
2020-04-10 16:50:18 -0400

    Blanking a bit

--- src/assets/map/buildings.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="vx-buildingstileset" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="building.png" width="512" height="512"/>
-</tileset>
--- src/assets/map/trees.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="tree-variations_0" tilewidth="32" tileheight="32" tilecount="144" columns="16">
- <image source="trees.png" width="512" height="289"/>
-</tileset>
